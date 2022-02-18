7b94a947943e789dbafbda343886ac0ef7f07cd8
Schleimfresse <87274892+Schleimfresse@users.noreply.github.com>
2022-02-18 16:15:46 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,15 +4,6 @@ var menustatus = false;
 
 setTimeout(
   (window.load = function () {
-    var content = document.getElementById("onscroll");
-    content.style.opacity = 1;
-    content.style.transform = "translateX(" + 0 + "vw)";
-  }),
-  delayInMilliseconds
-);
-
-setTimeout(
-  (window.load = function () {
     let content = document.querySelector("header");
     content.style.transform = "translateY(" + 0 + "vh)";
   }),
@@ -78,4 +69,40 @@ function hide() {
   box = document.getElementById("mobile").style.left = -100 + "vw";
   menustatus = false;
 }
+setTimeout(() => {
+WhatAnimation("moveleft");
 
+$(window).scroll(function() {
+    WhatAnimation("moveleft");
+});
+
+function WhatAnimation(name) {
+    $("." + name).each(function() {
+        switch (name) {
+            case "fadescroll":
+                AddClass(this, "fade");
+                break;
+            case "moveleft":
+                AddClass(this, "left");
+                break;
+        }
+    });
+}
+
+function AddClass(object, name) {
+    if (IsVisible(object)) {
+        $(object).addClass(name);
+    } else {
+        $(object).removeClass(name);
+    }
+}
+
+function IsVisible(object) {
+    var viewport = $(window).scrollTop() + $(window).height();
+    var rand = $(object).offset();
+    rand.bottom = rand.top + $(object).outerHeight();
+    return !(
+        viewport < rand.top + 250 || $(window).scrollTop() > rand.bottom - 250
+    );
+}
+}, delayInMilliseconds);
